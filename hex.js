9444ef372b6ed16c7592f26ada9c907aa78e1236
yusuf963 <mohammad963yusuf@gmail.com>
2020-12-22 17:07:47 +0000

    adding indentation to files

--- hex.js
+++ hex.js
@@ -1,19 +1,17 @@
-const hex =[0, 1, 2, 3, 4, 5, ,6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
+const hex = [0, 1, 2, 3, 4, 5, , 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-const btn = document.getElementById('btn')
-const colorName = document.getElementById('colorName')
+const btn = document.getElementById("btn");
+const colorName = document.getElementById("colorName");
 
-btn.addEventListener('click', ()=>{
- let hexColor = '#';
- for(let i =0; i<6; i++){
-   hexColor+= hex[generateColor()]
- }
- colorName.textContent = hexColor;
- document.body.style.backgroundColor = hexColor
+btn.addEventListener("click", () => {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[generateColor()]
+  }
+  colorName.textContent = hexColor
+  document.body.style.backgroundColor = hexColor
 })
 
-
-
-const generateColor =()=>{
+const generateColor = () => {
   return Math.floor(Math.random() * hex.length)
 }
