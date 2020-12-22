ef898934cef14b00dfde2c4efb33a6e009b57ffa
yusuf963 <mohammad963yusuf@gmail.com>
2020-12-22 17:36:06 +0000

    adding new colors to colors array

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-const colors = ['green', 'red', 'blue', 'orange', '#000']
+const colors = ['green', 'red', 'blue', 'orange', '#000', 'aero', 'blueviolet', 'azure', "aliceBlue", 'YellowGreen', 'White', 'Bisque', 'AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Beige', 'Bisque', 'BlanchedAlmond', 'Brown', 'BlueViolet', 'BurlyWood', 'CadetBlue', 'Chartreuse', 'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan', 'DarkBlue', 'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGrey', 'DarkGreen', 'DarkKhaki', 'DarkMagenta', 'DarkOliveGreen', 'DarkOrange', 'DarkOrchid', 'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue', 'DarkSlateGray', 'DarkSlateGrey', 'DarkTurquoise']
 
 const btn = document.getElementById('btn')
 const colorName = document.getElementById('colorName')
